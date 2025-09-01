bb301842b808c77cd65936b2bbb8e5aa21855440
geck1942 <aurelien.jacquot@hotmail.fr>
2025-09-01 14:03:54 +0200

    ci: 🧑‍🔧 fixed 2 version release script

--- .github/actions/automated/increment-semver.sh
+++ .github/actions/automated/increment-semver.sh
@@ -24,7 +24,7 @@ done
 # Try to extract version with quotes first, then without quotes if not found
 current_version=$(awk -F'"' '/"version":/ {print $4; exit}' $file_path)
 if [ -z "$current_version" ]; then
-    current_version=$(awk '/Version:/ {print $2; exit}' $file_path)
+    current_version=$(awk '/Version:/ {print $4; exit}' $file_path)
 fi
 
 if [ -z "$current_version" ]; then
